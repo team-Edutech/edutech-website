2f26ec4a1152a4dfd00e5df6347eb589a49f9f96
Astariul <remondnicola@gmail.com>
2022-08-08 16:29:52 +0900

    :rocket: Deploy a real DB on railway

--- api/db/migrations/20220803084055_add_course/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Course" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "video" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
